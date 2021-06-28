b44720f56fb7d262a8fb80d8077dc88e96812c74
Marco Ermini <markoer73@users.noreply.github.com>
2021-06-28 08:20:36 +0200

    Updated documents and script

--- Project 3 - SDN-Based DoS Attacks and Mitigation/run_pox3.sh
+++ Project 3 - SDN-Based DoS Attacks and Mitigation/run_pox3.sh
@@ -3,6 +3,6 @@ sudo ./pox.py openflow.of_01 \
 	--port=6655 pox.forwarding.l3_learning \
 	pox.forwarding.Lab3Firewall \
 	--l2config="l2firewall.config" \
-        --l3config="l3firewall.config" \
+    --l3config="l3firewall.config" \
 	log.level --DEBUG
 
